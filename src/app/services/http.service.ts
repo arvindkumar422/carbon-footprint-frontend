4f3b10119a5f802ffdc839c57215e6fdcdabe6e0
BrundaSreedhar <brundasreedhar@gmail.com>
2020-06-19 14:50:21 +0530

    Add flight as a mode

--- src/app/services/http.service.ts
+++ src/app/services/http.service.ts
@@ -9,13 +9,13 @@ export class HttpService {
 
   constructor(private httpClient: HttpClient) { }
 
-  computeDistance(origins: Location, destinations: Location, mode: string, vehicle: string) {
+  computeEmissions(origins: Location, destinations: Location, mode: string, vehicle: string, seatType: string) {
     let originString: string = origins.latitude.toString() + "," + origins.longitude.toString();
     let destString: string = destinations.latitude.toString() + "," + destinations.longitude.toString();
     console.log("Src: ", originString);
     console.log("Dest: ", destString);
     return this.httpClient.post('http://localhost:3000/getDistance',
-      { "origins": [originString], "destinations": [destString], "mode": mode, "vehicle": vehicle });
+      { "origins": [originString], "destinations": [destString], "mode": mode, "vehicle": vehicle,"seatType":seatType });
   }
 
   getTweets() {
